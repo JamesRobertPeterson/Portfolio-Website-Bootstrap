c7f66d3dcb4e66d06a3ea5f3686a8b158d502d27
James Peterson <james.peterson@tesco.com>
2018-02-22 12:20:13 +0000

    Stable 1
    Starting again on Github

---
+++ script.js
@@ -0,0 +1,13 @@
+<div class="container">
+    <div class="header"><span>Expand</span>
+
+    </div>
+    <div class="content">
+        <ul>
+            <li>This is just some random content.</li>
+            <li>This is just some random content.</li>
+            <li>This is just some random content.</li>
+            <li>This is just some random content.</li>
+        </ul>
+    </div>
+</div>
